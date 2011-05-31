c630400a82f498d2476ba903c827a9b55171f594
art laptop <sorabji52@gmail.com>
2011-05-31 15:12:37 -0700

    test files for js, messing around a bit with gdb

---
+++ javascript/tests.js
@@ -0,0 +1,23 @@
+
+function multiplyTable(){
+    document.write("<table border=\"1\">");
+    var i=1;
+    var j=1;
+    for (i;i<=10;i++){
+	document.write("<tr>");
+	for (j;j<=10;j++){
+	    document.write("<td>");
+	    document.write(j*i);
+	    document.write("</td>");
+	}
+    j=1;
+    }
+    return 0;
+}
+
+
+function changeColor(tochange){
+    setTimeout("alert(\"yoseph\")",3000);
+}
+
+
