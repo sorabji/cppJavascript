9e1557fc623a0af4d861c978fbe69199949d7bc6
art homecomp <sorabji52@gmail.com>
2011-06-06 17:24:27 -0700

    all set to turn in first project

--- cpp/64/1project/Main.cpp
+++ cpp/64/1project/Main.cpp
@@ -12,14 +12,14 @@ int main()
   double area = 0;
   int size = 3;
   Shape *shapes[size];
-  shapes[0] = new Circle(5);
+  shapes[0] = new Circle(7);
   shapes[1] = new Rectangle(5,10);
   shapes[2] = new RightTriangle(5,10);
 
   for (int i=0 ; i < size; i++)
     {
       area = shapes[i]->calcArea();
-      cout << "area of shape #" << i << " is: "
+      cout << "The area of shape #" << i+1 << " is: "
 	   << area << endl;
 
       delete shapes[i];
